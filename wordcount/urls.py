e6d1981634a683066fef8627a53ca4dc134d64f7
Unknown <unknown@example.com>
2020-04-10 13:52:47 +0800

    添加了关于本站的网页

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import myfunction
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ha', myfunction.home),
-    path('count/', myfunction.count)
+    path('', myfunction.home),
+    path('count/', myfunction.count),
+    path('about/', myfunction.about)
 ]
